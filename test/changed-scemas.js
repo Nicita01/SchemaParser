d4ed2d5afc4a1729bc9c5790d65e5e7f894e0151
Nicita01 <nicitamac@gmail.com>
2018-07-24 02:46:43 +0300

    Add changed schemas for tests

---
+++ test/changed-scemas.js
@@ -0,0 +1,35 @@
+'use strict';
+
+// For tests
+
+module.exports = {
+  handshake1: {
+    version: '2b',
+    status: '1b',
+    reserved: '1b',
+    token: '36b'
+  },
+  handshake2: {
+    version: '2b',
+    status: '1b',
+    reserved: '1b',
+    token: '4b'
+  },
+  handshake3: {
+    status: '1b',
+    token: '4b'
+  },
+  handshake4: {
+    version: '2b',
+    status: '2b',
+    reserved: '1b',
+    token: '8b'
+  },
+  handshake5: {
+    version: '2b',
+    status: '1b',
+    reserved: '1b',
+    token: '16b',
+    someOtherToken: "64b"
+  },
+}
